b1af2c5b8cad4c43d493a40e38c640632a771df2
Rafaela Borges <rafasborgesw@gmail.com>
2024-09-29 17:09:15 -0300

    test: widget test

---
+++ test/home_test.dart
@@ -0,0 +1,18 @@
+import 'package:estilizacao_componentes/data/bank_inherited.dart';
+import 'package:estilizacao_componentes/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+    'My widget has a text "spent"',
+    (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: BankInherited(
+          child: Home(),
+        ),
+      ));
+      expect(find.text("Spent"), findsOneWidget);
+    },
+  );
+}
